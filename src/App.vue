c24342ea4d354bec786c3731305e9da1e443a866
loicbdev <loicbodolec@msn.com>
2021-02-19 16:07:02 +0100

    only tabulator

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Sorting Tables in a Vue CLI project</h1>
     <p>
-      Demo : Databases / Tabulator / Grid.js
+      Demo : Tabulator
     </p>
-    <h2>Sorting with Datatables</h2>
-    <small>Note that the Datatables plugin depends on jQuery!</small>
-    <sortable-datatable></sortable-datatable>
     <h2>Sorting using Tabulator</h2>
     <sortable-tabulator></sortable-tabulator>
-    <h2>Sorting using Gridjs.io</h2>
-    <sortable-grid></sortable-grid>
   </div>
 </template>
 
 <script>
-import SortableDatatable from './components/SortableDatatable'
 import SortableTabulator from './components/SortableTabulator'
-import SortableGrid from './components/SortableGrid'
 export default {
   name: 'app',
   components: {
-    SortableDatatable,
-    SortableTabulator,
-    SortableGrid
+    SortableTabulator
   }
 }
 </script>
